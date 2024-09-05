a83d1381831047efcd6979a78b626be73c373d14
Mario <3943008660101@ingenieria.usac.edu.gt>
2024-09-05 07:43:20 -0600

    Busqueda por dpi y nombre

--- main.py
+++ main.py
@@ -58,10 +58,13 @@ class main():
         exportarSalida(prueba, 'Salidas/' + nombre_busqueda +'.csv')
         exportarJSON(prueba, 'Salidas/' + nombre_busqueda +'.json')
 
-        dpi = input("Ingrese el dpi a buscar: ")
-        nombre = input("Ingrese el nombre a buscar: ")
-
-        print(self.arbol.buscar_por_nombre_y_dpi(dpi, nombre.lower()))
+        while True:
+            dpi = input("Ingrese el dpi a buscar: ")
+            nombre = input("Ingrese el nombre a buscar: ")
+            print(self.arbol.buscar_por_nombre_y_dpi(dpi, nombre.lower()))
+            res = input("¿Desea realizar otra busqueda? ")
+            if res.lower() != "si":
+                break
 
 if __name__ == '__main__':
     main()
